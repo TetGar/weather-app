f1ffdde8ada0bc4d347d4bca542a7dd90f60ac47
TetGar <109559114+TetGar@users.noreply.github.com>
2022-09-06 19:38:28 +0100

    added script to project

---
+++ index.js
@@ -0,0 +1,23 @@
+let currentDate = new Date();
+
+let dayOfTheWeek = document.querySelector("#dayOfTheWeek");
+
+let hours = currentDate.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = currentDate.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[currentDate.getDay()];
+dayOfTheWeek.innerHTML = `${day} , ${hours}:${minutes}`;
